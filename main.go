1795e34fcf7dbc8c872c2005fbe123513f390421
Orinameh <factomeme@gmail.com>
2020-10-10 14:45:24 +0100

    golang api

--- main.go
+++ main.go
@@ -1,11 +1,79 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"offersapp/models"
+	"offersapp/routes"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+	"github.com/jackc/pgx/v4"
+)
 
 func main() {
+	conn, err := connectDB()
+	if err != nil {
+		return
+	}
 	router := gin.Default()
+	router.Use(dbMiddleware(*conn))
 
-	// usersGroup := router.Group("users")
+	usersGroup := router.Group("users")
+	{
+		usersGroup.POST("register", routes.UsersRegister)
+		usersGroup.POST("login", routes.UsersLogin)
+	}
+
+	itemsGroup := router.Group("items")
+	{
+		itemsGroup.GET("all", routes.GetItems)
+		itemsGroup.POST("create", authMiddleware(), routes.ItemsCreate)
+		itemsGroup.GET("sold_by_user", authMiddleware(), routes.ItemsForSaleByCurrentUser)
+		itemsGroup.PUT("update", authMiddleware(), routes.ItemsUpdate)
+
+	}
 
 	router.Run()
+}
+
+func connectDB() (c *pgx.Conn, err error) {
+	conn, err := pgx.Connect(context.Background(), "postgresql://davidevhade:0704502@localhost:5432/offersapp")
+	if err != nil {
+		fmt.Println("Error connecting to DB")
+		fmt.Println(err.Error())
+	}
+	_ = conn.Ping(context.Background())
+	return conn, err
+}
+
+func dbMiddleware(conn pgx.Conn) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Set("db", conn)
+		c.Next()
+	}
+}
+
+func authMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		bearer := c.Request.Header.Get("Authorization")
+		split := strings.Split(bearer, "Bearer ")
+		if len(split) < 2 {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "Not authenticated. "})
+			c.Abort()
+			return
+		}
+
+		token := split[1]
+		isValid, userID := models.IsTokenValid(token)
+		if isValid == false {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "Not authenticated. "})
+			c.Abort()
+		} else {
+			// if valid, set user_id to the gin context
+			c.Set("user_id", userID)
+			c.Next()
+		}
+	}
 }
---
+++ models/user.go
@@ -0,0 +1,122 @@
+package models
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"strings"
+	"time"
+
+	"github.com/dgrijalva/jwt-go"
+	"github.com/gofrs/uuid"
+	"github.com/jackc/pgx/v4"
+	"golang.org/x/crypto/bcrypt"
+)
+
+var (
+	tokenSecret = []byte(os.Getenv("TOKEN_SECRET"))
+)
+
+// User struct to define the user model
+type User struct {
+	ID              uuid.UUID `json:"id"`
+	CreatedAt       time.Time `json:"_"`
+	UpdatedAt       time.Time `json:"_"`
+	Email           string    `json:"email"`
+	PasswordHash    string    `json:"-"`
+	Password        string    `json:"password"`
+	PasswordConfirm string    `json:"password_confirm"`
+}
+
+// Register function(interface) implemented by a fucntion that accepts user struct
+func (u *User) Register(conn *pgx.Conn) error {
+	if len(u.Password) < 4 || len(u.PasswordConfirm) < 4 {
+		return fmt.Errorf("Password must be at least 4 characters long")
+	}
+
+	if u.Password != u.PasswordConfirm {
+		return fmt.Errorf("Password must be at least 4 characters long")
+	}
+
+	if len(u.Email) < 4 {
+		return fmt.Errorf("Email must be at least 4 characters long")
+	}
+	//convert email to lower case always
+	u.Email = strings.ToLower(u.Email)
+
+	// check uniqueness of email
+	row := conn.QueryRow(context.Background(), "SELECT id from user_account WHERE email = $1", u.Email)
+	userLookup := User{}
+	err := row.Scan(&userLookup)
+	if err != pgx.ErrNoRows {
+		fmt.Println("found user")
+		fmt.Println(userLookup.Email)
+		return fmt.Errorf("A user with that email already exists")
+
+	}
+
+	pwdHash, err := bcrypt.GenerateFromPassword([]byte(u.PasswordConfirm), bcrypt.DefaultCost)
+	if err != nil {
+		return fmt.Errorf("There was an error creating your account")
+	}
+	u.PasswordHash = string(pwdHash)
+
+	// save user to db
+	now := time.Now()
+	_, err = conn.Exec(context.Background(), "INSERT INTO user_account (created_at, updated_at, email, password_hash) VALUES($1, $2, $3, $4)", now, now, u.Email, u.PasswordHash)
+
+	return err
+}
+
+// GetAuthToken returns the auth token to be used
+func (u *User) GetAuthToken() (string, error) {
+	claims := jwt.MapClaims{}
+	claims["authorized"] = true
+	claims["user_id"] = u.ID
+	claims["exp"] = time.Now().Add(time.Hour * 24).Unix()
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, &claims)
+	authToken, err := token.SignedString(tokenSecret)
+	return authToken, err
+}
+
+// IsAuthenticated checks to make sure password is correct and user can login
+func (u *User) IsAuthenticated(conn *pgx.Conn) error {
+	row := conn.QueryRow(context.Background(), "SELECT id, password_hash from user_account WHERE email = $1", u.Email)
+	err := row.Scan(&u.ID, &u.PasswordHash)
+	if err == pgx.ErrNoRows {
+		fmt.Println("user with email not found")
+		return fmt.Errorf("Invalid login credentials")
+	}
+
+	err = bcrypt.CompareHashAndPassword([]byte(u.PasswordHash), []byte(u.Password))
+	if err != nil {
+		return fmt.Errorf("Invalid login credentials")
+	}
+
+	return nil
+}
+
+// IsTokenValid function is used to check if a token is valid
+func IsTokenValid(tokenString string) (bool, string) {
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); ok == false {
+			return nil, fmt.Errorf("Token signing method is not valid: %v", token.Header["alg"])
+		}
+
+		return tokenSecret, nil
+	})
+
+	if err != nil {
+		fmt.Printf("Err %v\n", err)
+		return false, ""
+	}
+
+	if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+		userID := claims["user_id"]
+		return true, userID.(string)
+	} else {
+		fmt.Printf("The alg header %v\n", claims["alg"])
+		fmt.Println(err)
+		return false, "uuid.UUID{}"
+	}
+}
